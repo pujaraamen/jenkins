0a5aea3908586e266ce68baa1c946f946698b141
aniket <ubuntu@ip-172-31-9-239.ap-south-1.compute.internal>
2026-08-25 07:30:24 +0000

    commit 9th

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello from Aniket and Ajinkya Bari hi Docker Agent 🚀"
+def home():
+    return "Hello from Aniket and Ajinkya Bari! 👋🐳"
+
+@app.route("/about")
+def about():
+    return "This Flask application is running inside Docker."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
