2cecca26601a0c7185db3263d5aa9fec17175d63
aniket <ubuntu@ip-172-31-9-239.ap-south-1.compute.internal>
2026-08-25 07:19:24 +0000

    august 25 commit with change 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Aniket and Ajinkya Docker Agent 🚀"
+    return "Hello from Aniket and Ajinkya Bari Docker Agent 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
